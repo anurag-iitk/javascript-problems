433b038bdb971f490f43131dfc32c16d58a9c162
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2023-01-20 22:09:48 +0530

    Traversal an array

---
+++ Traverse_array.js
@@ -0,0 +1,16 @@
+// Traversal array
+
+let i = 0;
+
+var arr = [1, 2, 3, 4, 5, 6];
+
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+i = 0;
+
+while (i < arr.length) {
+  console.log(`While loop: ${arr[i]}`);
+  i++;
+}
