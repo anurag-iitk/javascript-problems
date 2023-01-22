a323deb2c4450228d716cb1c12386e2e9d7d1a9e
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2023-01-22 13:53:36 +0530

    Queue operation using class
    #FIFO Or #LILO
    1. Enqueue- Store element in the queue
    2. Dequeue- Remove the element in rear
    3. Display
    4. Add- Pass to array to stored value in queue

---
+++ Queue.js
@@ -0,0 +1,57 @@
+// Queue operations using class
+
+class Queue {
+    item = [];
+    currentSize;
+    maxSize;
+
+    constructor(size){
+        this.maxSize = size;
+        this.currentSize = this.item.length;
+    }
+
+// Add element in queue
+    enqueue(value){
+        if( this.currentSize < this.maxSize ){
+            this.item[this.currentSize] = value;
+            this.currentSize++;
+        } else {
+            console.log("Queue is full");
+        }
+    }
+
+// Remove element in queue
+    dequeue(){
+        if( this.currentSize == 0){
+            console.log("Queue is empty");
+        } else {
+            for( let i=0; i<this.item.length; i++){
+                this.item[i] = this.item[i+1];
+            }
+            this.item.length -= 1;
+        }
+    }
+
+// Display items of the queue
+    display(){
+        console.log(this.item);
+    }
+
+// Using pass an array to enqueue
+    addArray(arr){
+        for( let i=0; i<arr.length; i++){
+            this.enqueue(arr[i]);
+        }
+    }
+
+}
+
+obj = new Queue(5);
+arr = [1,2,3,4,5]
+obj.addArray(arr);
+// obj.enqueue(2);
+// obj.enqueue(3);
+// obj.enqueue(4);
+obj.dequeue();
+obj.dequeue();
+obj.display();
