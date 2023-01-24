e54b98a16f34728de674ed4f7d84b09858d49dad
Anurag Saini <71720957+anuragsaini2472002@users.noreply.github.com>
2023-01-24 16:03:05 +0530

    Map data structure

---
+++ Map.js
@@ -0,0 +1,10 @@
+// Map Data Structure
+
+const data = new Map([
+    ["firstName", "anurag"],
+    ["lastName", "saini"]
+]);
+
+console.log(data.size);
+console.log(data.has("firstName"));
+console.log(data.get("firstName"));
